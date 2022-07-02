9653b9cf9fe5624a90fed5bc22adb28b7e1140a7
kentaro ishida <ken.babuo@gmail.com>
2022-07-02 17:05:26 +0900

    モジュールの分割

---
+++ src/modules/types/questionTypes.ts
@@ -0,0 +1,8 @@
+import Inference from "../../inference";
+import ProfitLoss from "../../profitLoss";
+import Tsurukame from "../../tsurukame";
+import WorkCalc from "../../workCalc";
+
+type questionTypes = Tsurukame | Inference | ProfitLoss | WorkCalc;
+
+export default questionTypes;
